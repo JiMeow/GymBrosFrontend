513aac9fb74e1fa0f9ea27c4d3ac471b3c8ace26
Jirat Meewassana <64889751+JiMeow@users.noreply.github.com>
2023-11-20 01:40:45 +0700

    Add Landing page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,11 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 
 import "@/styles/globals.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import DevViewport from "@/components/DevViewport";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -11,6 +16,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      {process.env.NODE_ENV === "development" && <DevViewport />}
     </SessionProvider>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
+import { Button, Typography } from "@mui/material";
+import { PaperPlaneRight } from "@phosphor-icons/react";
 export default function Home() {
   return (
-    <div className="grid h-full min-h-[100vh] place-items-center bg-yellow-400 text-4xl font-bold">
-      GYM BroS
-    </div>
+    <>
+      <div className="h-full min-h-[100vh] bg-yellow-400 p-8 px-12">
+        <div className="flex h-full flex-col items-center justify-center sm:min-h-[100vh] ">
+          <Typography className="text-center text-lg font-bold sm:my-4 sm:text-5xl">
+            Welcome to GymBros
+          </Typography>
+          <Typography className="mt-4 text-center text-lg font-[500] sm:my-4 sm:text-3xl">
+            Start Your Fitness Journey with Us!
+          </Typography>
+          <Typography className="mt-4 indent-6 text-base sm:my-4 sm:w-[80%] sm:max-w-5xl sm:text-center sm:indent-0 sm:text-lg">
+            Discover tailor-made workout plans, expert guidance, and connect
+            with a supportive community committed to a healthier lifestyle.
+            Whether you{"'"}re a seasoned pro or a beginner, GymBros is your
+            ultimate fitness companion. Ready to transform? Let{"'"}s take the
+            first step together.
+          </Typography>
+          <Button
+            variant="contained"
+            endIcon={<PaperPlaneRight />}
+            className="mt-10 w-fit bg-black text-white "
+          >
+            Getting Started
+          </Button>
+        </div>
+      </div>
+    </>
   );
 }

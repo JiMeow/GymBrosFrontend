d32ab61afb75fb0ab495c036ec67c3b6316e873d
Jirat Meewassana <64889751+JiMeow@users.noreply.github.com>
2023-11-20 00:12:11 +0700

    Init projects

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="grid h-full min-h-[100vh] place-items-center bg-yellow-400 text-4xl font-bold">
+      GYM BroS
+    </div>
+  );
+}

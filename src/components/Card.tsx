d093de094d67f0862268138175bc7040fce6f929
Jirat Meewassana <64889751+JiMeow@users.noreply.github.com>
2023-11-24 03:20:05 +0700

    Change card color

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,14 +13,14 @@ const Card: React.FC<CardProps> = ({ title, description, image }) => {
     <>
       <div
         className="flex h-24 flex-none flex-row items-center gap-2 rounded-md border-2
-      border-white bg-gray-300 p-1 text-black sm:h-32 sm:gap-4 sm:border-4 sm:p-2"
+      border-white bg-white p-1 text-black brightness-90 sm:h-32 sm:gap-4 sm:border-4 sm:p-2 "
       >
         <Image
           src={image}
           width={1000}
           height={1000}
           alt="Card"
-          className="h-full w-20 rounded-md border-2 border-black bg-gray-300 object-cover object-center p-1 sm:w-24"
+          className="h-full w-20 rounded-md bg-white object-cover object-center p-1 sm:w-24"
         />
         <div className="flex w-24 flex-col items-center justify-center gap-1 sm:w-44 sm:gap-2">
           <Typography className="text-xs font-semibold sm:text-sm">

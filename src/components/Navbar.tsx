59b578cade230017a368a5587706a18619310774
Npwskp <nopawit.sukolpuk@gmail.com>
2023-12-02 14:51:02 +0700

    add nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,46 @@
 import { Typography } from "@mui/material";
 import { IoPersonCircleOutline } from "react-icons/io5";
-import React from "react";
+import { IoMdFitness } from "react-icons/io";
+import React, { use, useEffect, useState } from "react";
+import { Button } from "@nextui-org/react";
 
 const Navbar = () => {
+  const [opacity, setOpacity] = useState(100);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setOpacity(
+        100 - window.scrollY / 10 > 50 ? 100 - window.scrollY / 10 : 50,
+      );
+    });
+  }, [opacity]);
+  console.log(opacity);
+
   return (
     <>
-      <div className="left-0 top-0 m-auto flex justify-between bg-white p-0 px-2">
-        <Typography className="pl-2 text-center text-lg font-bold sm:my-4 sm:text-3xl">
+      <div
+        className={`sticky left-0 top-0 z-50 m-auto flex w-full justify-between p-4 px-6 `}
+        style={{
+          backgroundColor: `rgba(148, 145, 145,${opacity / 100})`,
+          transition: "all 0.5s ease",
+        }}
+      >
+        <div className=" my-auto text-center text-2xl font-bold italic text-white sm:text-3xl">
           GymsBro
-        </Typography>
-        <div className="flex flex-row items-center justify-center">
-          <IoPersonCircleOutline className="w-8" />
-          <Typography className="">Login</Typography>
+        </div>
+        <div className="flex flex-row items-center justify-center gap-4">
+          <Button
+            className="rounded-full bg-black px-2 py-2 text-white hover:bg-white hover:text-black"
+            startContent={<IoMdFitness />}
+          >
+            Log In
+          </Button>
+          <Button
+            className="rounded-full bg-black px-2 py-2 text-white hover:bg-white hover:text-black"
+            startContent={<IoPersonCircleOutline />}
+          >
+            Sign up
+          </Button>
         </div>
       </div>
     </>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { NextUIProvider } from "@nextui-org/react";
+import { DM_Sans } from "next/font/google";
 
 import "@/styles/globals.css";
 import "@fontsource/roboto/300.css";
@@ -9,18 +10,23 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import DevViewport from "@/components/DevViewport";
+const fonts = DM_Sans({
+  subsets: ["latin"],
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <NextUIProvider>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-        {process.env.NODE_ENV === "development" && <DevViewport />}
-      </SessionProvider>
-    </NextUIProvider>
+    <main className={fonts.className}>
+      <NextUIProvider>
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+          {process.env.NODE_ENV === "development" && <DevViewport />}
+        </SessionProvider>
+      </NextUIProvider>
+    </main>
   );
 };
 

8f4f024e5429ea2687606a56cc94b0333418ce51
Jirat Meewassana <64889751+JiMeow@users.noreply.github.com>
2023-12-04 18:10:07 +0700

    Change text depend on viewport

--- src/components/Mocktable.tsx
+++ src/components/Mocktable.tsx
@@ -2,33 +2,27 @@ import { usePlanByUserId } from "@/hooks/plan/usePlanByUserId";
 import React from "react";
 import MockTableCell from "./MockTableCell";
 
-const dayMap = {
-  Sunday: "Sun",
-  Monday: "Mon",
-  Tuesday: "Tue",
-  Wednesday: "Wed",
-  Thursday: "Thu",
-  Friday: "Fri",
-  Saturday: "Sat",
-};
-
 type Props = {
   id: string;
 };
 
 const Table: React.FC<Props> = ({ id }) => {
-  const { data: plans, error, isLoading } = usePlanByUserId({ id });
+  const { data: plans } = usePlanByUserId({ id });
   return (
     <>
       <div className="flex w-[90vw] flex-col gap-4 sm:w-[75vw]">
         {plans?.map((item) => {
-          // console.log(item?.exercise);
           return (
             <div
               className="flex flex-none flex-row items-center gap-1 sm:gap-4"
               key={item.id}
             >
-              <div className="text-sm sm:text-base">{item?.dayofweek}</div>
+              <div className="text-sm sm:hidden sm:text-base">
+                {getDayShorthand(item?.dayofweek)}
+              </div>
+              <div className="hidden text-sm sm:block sm:text-base">
+                {item?.dayofweek}
+              </div>
               <div className="flex flex-row gap-4 overflow-auto">
                 {item?.exercise?.map((e) => <MockTableCell key={e} id={e} />)}
               </div>
@@ -41,3 +35,24 @@ const Table: React.FC<Props> = ({ id }) => {
 };
 
 export default Table;
+
+function getDayShorthand(date: string): string {
+  switch (date) {
+    case "Sunday":
+      return "SUN";
+    case "Monday":
+      return "MON";
+    case "Tuesday":
+      return "TUE";
+    case "Wednesday":
+      return "WED";
+    case "Thursday":
+      return "THU";
+    case "Friday":
+      return "FRI";
+    case "Saturday":
+      return "SAT";
+    default:
+      return "Invalid Date";
+  }
+}
